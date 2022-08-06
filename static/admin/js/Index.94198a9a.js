95b64ddc52f69277fc63c73bbfab055074a0bfd7
wakk3107 <chenyulin3107@163.com>
2022-08-06 13:40:59 +0800

    Add files via upload

---
+++ static/admin/js/Index.94198a9a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["Index"],{"08f9":function(e,n,t){"use strict";t("3f2a")},"3f2a":function(e,n,t){},adf4:function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"welcome"},[t("br"),t("br"),t("br"),t("br"),t("h3",[e._v("欢迎来到 GINBLOG 后台管理页面")])])}],s=(t("08f9"),t("2877")),a={},u=Object(s["a"])(a,r,c,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=Index.94198a9a.js.map
